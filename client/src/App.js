584726a74ab53573b7bc1ec125c107ce957544fa
neon <78809835+n-eoncode@users.noreply.github.com>
2023-08-03 21:02:16 +0100

    pages folder deleted
    error with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import about from './Pages/about'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
