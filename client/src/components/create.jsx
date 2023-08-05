ea9cc53bc303076e8327a5b9b2ff6442bc9b6a18
neon <itsafam@gmail.com>
2023-08-05 07:27:37 +0100

    initialised backend
    created server folder/ ensured connection to server works

---
+++ client/src/components/create.jsx
@@ -0,0 +1,6 @@
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+
+
--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -6,15 +6,15 @@ function Main() {
   return (
     <div className="h-screen">
       <nav id="navBar">
-        <img src={logo} alt="LOGO" className="float-left w-20 my-1"></img>
+        <img src={logo} alt="LOGO" className="float-left w-20 my-2"></img>
         <h1 id="titleEcho" className="float-left select-none text-5xl font-bold my-2"><Link to = '/'>ECHO</Link></h1>
         <Link to = '/signup'>
-          <button id="BtnSignUp" class="float-right font-bold rounded-full bg-purple-400 hover:bg-purple-500 text-white w-32 h-12 my-2">
+          <button id="BtnSignUp" class="float-right font-bold rounded-full bg-purple-400 hover:bg-purple-500 text-white w-32 h-12 my-2 mx-2">
             Sign Up
           </button>
         </Link>
         <Link to = '/signin'>
-          <button id="BtnSignin" class="float-right font-bold rounded-full bg-indigo-800 hover:bg-indigo-900 text-white w-32 h-12 my-2"> 
+          <button id="BtnSignin" class="float-right font-bold rounded-full bg-indigo-800 hover:bg-indigo-900 text-white w-32 h-12 my-2 mx-2"> 
             Sign In
           </button>
         </Link> 
