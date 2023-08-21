55aca84832fa3b4fbc8afc34d9382a4c7e159823
neon <itsafam@gmail.com>
2023-08-21 21:05:54 +0100

    created Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Main from './pages/main';
 import SignIn from './pages/signin';
 import SignUp from './pages/signup';
 import NoPage from './pages/nopage';
 import MainFeed from './pages/mainfeed';
+import NavBar from "./components/navbar.jsx"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Main />} /> 
-        <Route path="/signin" element={<SignIn />} />
-        <Route path = '/signup' element = {<SignUp />} />
-        <Route path = '/nopage' element = {<NoPage /> } />
-        <Route path = '/mainfeed' element = {<MainFeed />} />
-      </Routes>
-    </Router>
+    <div className = "App">
+      <BrowserRouter>
+        <NavBar />
+        <div className = "pages"> 
+          <Routes>
+            <Route exact path="/" element={<Main />} /> 
+            <Route path="/signin" element={<SignIn />} />
+            <Route path = '/signup' element = {<SignUp />} />
+            <Route path = '/nopage' element = {<NoPage /> } />
+            <Route path = '/mainfeed' element = {<MainFeed />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
     
   );
 }
