51a9a325ef16ca81369ba141725e4135a788545c
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-08-29 20:44:28 +0100

    navbar tweaks

--- client/src/pages/mainfeed.jsx
+++ client/src/pages/mainfeed.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import '../index.css';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useNavigate } from "react-router-dom"
 
 const MainFeed = () => { 
-  return (
-    <div className = 'h-screen'>
-    
-    </div>
-  ); 
+
+  const { user } = useAuthContext()
+  const navigate = useNavigate()
+
+  if (!user) {
+    navigate("/login")
+
+  } else {
+      return (
+        <div className = 'h-screen'>
+        
+        </div>
+    ); 
+
+  }
 };
 
 export default MainFeed;
