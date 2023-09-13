868ecae27086e3ffce741a34a6eca4249f1076bf
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-09-13 16:02:36 +0100

    update server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors( {
-    origin: 'https://super-concha-7027eb.netlify.app', 
+    origin: 'https://starlit-strudel-284b11.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true,
 } ));
