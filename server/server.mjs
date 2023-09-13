b8cf6389cba487313e167fc6ae642fcc9b0a4b1e
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-09-13 13:46:52 +0100

    vercel setup

--- server/server.mjs
+++ server/server.mjs
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors( {
-    origin: 'http://localhost:3000', 
+    origin: 'https://echo-zo5u.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true,
 } ));
