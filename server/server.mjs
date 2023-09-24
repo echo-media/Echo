0f6b3c802576ccfaa2bb8199018a1086a166e3b9
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-09-24 21:40:41 +0100

    minor tweak

--- server/server.mjs
+++ server/server.mjs
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors( {
     origin: 'http://localhost:3000', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
